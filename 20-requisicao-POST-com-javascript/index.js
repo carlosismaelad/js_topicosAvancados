faa0579e40b9daf99c166b7aea754d52f909c95d
carlosismaelad <carlosismaelad@outlook.com>
2023-09-19 17:34:44 -0300

    Finalizando com a criação do método POST

--- 20-requisicao-POST-com-javascript/index.js
+++ 20-requisicao-POST-com-javascript/index.js
@@ -29,3 +29,26 @@ async function fetchArticles() {
 document.addEventListener('DOMContentLoaded', () => {
     fetchArticles()
 })
+
+const form = document.querySelector('form')
+form.addEventListener('submit', async ev => {
+    ev.preventDefault()
+
+    const articleData = {
+        title: document.querySelector('#title').value,
+        author: document.querySelector('#author').value,
+        content: document.querySelector('#content').value
+    }
+
+    const response = await fetch('http://localhost:3000/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(articleData)
+    })
+
+    const savedArticle = await response.json()
+    form.reset()
+    renderArticles(savedArticle)
+})
